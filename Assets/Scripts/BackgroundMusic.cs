3f144ff88464d9eec25f05db44d4a1b24b10aaf1
eythorsnaer <eythorsnaer@gmail.com>
2017-03-15 11:54:32 +0000

    menu music and play button

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour {
+
+	private static BackgroundMusic instance = null;
+	public static BackgroundMusic Instance {
+		get { return instance; }
+	}
+
+	AudioSource audio;
+
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	void Start() {
+		audio = GetComponent<AudioSource> ();
+	}
+
+	public void Mute() {
+		audio.mute = !audio.mute;
+	}
+}
