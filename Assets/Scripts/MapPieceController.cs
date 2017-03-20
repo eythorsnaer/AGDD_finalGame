9858565a02933cf972b263c4e8af513187c802ad
SwagMal <bjarturhafthors@hotmail.com>
2017-03-20 21:45:13 +0000

    Map pieces

--- Assets/Scripts/MapPieceController.cs
+++ Assets/Scripts/MapPieceController.cs
@@ -2,15 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapPieceController : MonoBehaviour {
+public class MapPieceController : MonoBehaviour
+{
+    public float hoverHeight;
+    public float hoverSpeed;
+    private Rigidbody2D rb2d;
+    private float yOrigin;
+    private int lastFLoatDirection;
+    private bool destroy;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        yOrigin = gameObject.transform.position.y;
+        lastFLoatDirection = -1;
+    }
+
+    void FixedUpdate()
+    {
+        hover();
+    }
+
+    void hover()
+    {
+        if (gameObject.transform.position.y - yOrigin >= hoverHeight)
+        {
+            rb2d.velocity = new Vector2(0, -hoverSpeed);
+            lastFLoatDirection = -1;
+        }
+        else if (yOrigin - gameObject.transform.position.y >= hoverHeight)
+        {
+            rb2d.velocity = new Vector2(0, hoverSpeed);
+            lastFLoatDirection = 1;
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(0, lastFLoatDirection * hoverSpeed);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
 }
