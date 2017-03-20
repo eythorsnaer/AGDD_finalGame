142775fdb6005903f7d84d5c00dc85faaf99105e
SwagMal <bjarturhafthors@hotmail.com>
2017-03-20 19:12:07 +0000

    Save and load functions

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class GameController : MonoBehaviour {
+    public static GameController controller;
+
+	void Awake()
+    {
+        if (controller == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            controller = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gameProgress.dat");
+
+        GameData data = new GameData();
+        // set data as data
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/gameProgress.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gameProgress.dat", FileMode.Open);
+            GameData data = (GameData)bf.Deserialize(file);
+            file.Close();
+        }
+    }
+}
+
+[System.Serializable]
+class GameData
+{
+
+} 
