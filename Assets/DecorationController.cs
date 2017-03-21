9854e58f335594a5243796f42098c13c2b033f45
SwagMal <bjarturhafthors@hotmail.com>
2017-03-21 20:47:45 +0000

    Level update

---
+++ Assets/DecorationController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecorationController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int spriteNumber = Random.Range (1, 4);
+		string path = "Sprites/Environment/Decorations/Paraphernalia" + spriteNumber.ToString ();
+		gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> (path);
+	}
+}
