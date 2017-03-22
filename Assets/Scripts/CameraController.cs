2cffaedcb3ba2063fda141b93fc3c04b3f781499
eythorsnaer <eythorsnaer@gmail.com>
2017-03-22 02:33:09 +0000

    parallax fix

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,7 +17,7 @@ public class CameraController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Screen.SetResolution(1920, 1080, true);
-		target = GameObject.Find("Player").transform;
+		target = GameObject.FindGameObjectWithTag("Player").transform;
     }
  
      // Update is called once per frame
