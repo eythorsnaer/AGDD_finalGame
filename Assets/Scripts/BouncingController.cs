727446a4a6a3c5e6b8c1e58473aaac77ac96685c
eythorsnaer <eythorsnaer@gmail.com>
2017-03-30 21:09:38 +0000

    mushroom anim

---
+++ Assets/Scripts/BouncingController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncingController : MonoBehaviour {
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Player") {
+			Debug.Log("baaa");
+			anim.SetTrigger("Jump");
+		}
+	}
+
+
+}
