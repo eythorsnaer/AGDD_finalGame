363fddc66191f8d01140e93d2fffa1a0f8a4fd59
SwagMal <bjarturhafthors@hotmail.com>
2017-03-30 14:48:07 +0000

    Double escape apparently didn't go through

--- Assets/Scripts/Menu/PauseGame.cs
+++ Assets/Scripts/Menu/PauseGame.cs
@@ -55,6 +55,7 @@ public class PauseGame : MonoBehaviour {
 	}
 
 	public void ExitToMainMenu() {
+		Time.timeScale = 1;
 		SceneManager.LoadScene (0);
 		//Application.Quit();
 		//UnityEditor.EditorApplication.isPlaying = false;
